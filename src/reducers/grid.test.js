1b6fba8b6e71e798d57f2671b0201fd0a06de91f
CharmedSatyr <z@charmed.tech>
2018-03-30 10:39:56 +0900

    +test for moveSouth on first cell of last row; rewrote some manual grid mocks with splice

--- src/reducers/grid.test.js
+++ src/reducers/grid.test.js
@@ -6,6 +6,7 @@ describe('grid reducer', () => {
   it('should toggle the state indicating the player character', () => {
     const initialState = [{ player: false }, { player: true }];
     const result = [{ player: true }, { player: false }];
+
     expect(grid(initialState, { type: 'TOGGLE_CELL', index: 0 })).toEqual(result);
   });
 
@@ -13,6 +14,7 @@ describe('grid reducer', () => {
   it('should advance the player property one index if not at boundary', () => {
     const initialState = [{ player: true }, { player: false }];
     const result = [{ player: false }, { player: true }];
+
     expect(grid(initialState, { type: 'MOVE_EAST', position: 0 })).toEqual(result);
   });
 
@@ -21,20 +23,18 @@ describe('grid reducer', () => {
     // manual mocks
     // last index is player
     const initialState = [];
-    for (let i = 0; i < TOTAL_CELLS - 1; i++) {
+    for (let i = 0; i < TOTAL_CELLS; i++) {
       initialState.push({ player: false });
     }
-    initialState.push({ player: true });
+    initialState.splice(TOTAL_CELLS - 1, 1, { player: true });
 
     // end of second to last row is player
     const result = [];
-    for (let i = 0; i < TOTAL_CELLS - GRID_WIDTH - 1; i++) {
+    for (let i = 0; i < TOTAL_CELLS; i++) {
       result.push({ player: false });
     }
-    result.push({ player: true });
-    for (let i = 0; i < GRID_WIDTH; i++) {
-      result.push({ player: false });
-    }
+    result.splice(TOTAL_CELLS - 1 - GRID_WIDTH, 1, { player: true });
+
     expect(grid(initialState, { type: 'MOVE_NORTH', position: TOTAL_CELLS - 1 })).toEqual(result);
     expect(grid(initialState, { type: 'MOVE_NORTH', position: 0 })).toEqual(initialState);
   });
@@ -45,30 +45,40 @@ describe('grid reducer', () => {
 
     // end of second to last row is player
     const initialState = [];
-    for (let i = 0; i < TOTAL_CELLS - GRID_WIDTH - 1; i++) {
+    for (let i = 0; i < TOTAL_CELLS; i++) {
       initialState.push({ player: false });
     }
-    initialState.push({ player: true });
-    for (let i = 0; i < GRID_WIDTH; i++) {
-      initialState.push({ player: false });
-    }
+    initialState.splice(TOTAL_CELLS - 1 - GRID_WIDTH, 1, { player: true });
+
     // last index is player
     const result = [];
-    for (let i = 0; i < TOTAL_CELLS - 1; i++) {
+    for (let i = 0; i < TOTAL_CELLS; i++) {
       result.push({ player: false });
     }
-    result.push({ player: true });
+    result.splice(TOTAL_CELLS - 1, 1, { player: true });
+
+    // first cell on last row
+    const initialState2 = [];
+    for (let i = 0; i < TOTAL_CELLS; i++) {
+      initialState2.push({ player: false });
+    }
+    initialState2.splice(TOTAL_CELLS - GRID_WIDTH, 1, { player: true });
 
     expect(
-      grid(initialState, { type: 'MOVE_SOUTH', position: TOTAL_CELLS - GRID_WIDTH - 1 })
+      grid(initialState, { type: 'MOVE_SOUTH', position: TOTAL_CELLS - 1 - GRID_WIDTH })
     ).toEqual(result);
     expect(grid(result, { type: 'MOVE_SOUTH', position: TOTAL_CELLS - 1 })).toEqual(result);
+    // Ensure the first cell on the last row cannot move down
+    expect(grid(initialState2, { type: 'MOVE_SOUTH', position: TOTAL_CELLS - GRID_WIDTH })).toEqual(
+      initialState2
+    );
   });
 
   /*** MOVE_WEST ***/
   it('should move the player property back one index if not at boundary', () => {
     const initialState = [{ player: false }, { player: true }];
     const result = [{ player: true }, { player: false }];
+
     expect(grid(initialState, { type: 'MOVE_WEST', position: 1 })).toEqual(result);
     expect(grid(result, { type: 'MOVE_WEST', position: 0 })).toEqual(result);
   });
