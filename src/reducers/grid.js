324d1c9b51e8ee20b326df8e4b089d175a7d1101
CharmedSatyr <z@charmed.tech>
2018-03-30 10:39:06 +0900

    removed nearby/explored functions; fixed grid exceed on moveSouth

--- src/reducers/grid.js
+++ src/reducers/grid.js
@@ -31,62 +31,12 @@ const grid = (state = gridData, action) => {
         // Deactivate others
         // If it's not the player
         if (index !== action.index) {
-          /*** RIGHT ***/
-          // If index is one or two cells higher and not on another line
-          if (
-            (index === action.index + 1 && index % GRID_WIDTH !== 0) ||
-            (index === action.index + 2 && index % GRID_WIDTH !== 0 && index % GRID_WIDTH !== 1)
-          ) {
-            // It is nearby and explored
-            return Object.assign({}, cell, {
-              player: false,
-              nearby: true,
-              explored: true
-            });
-          }
-          // If it's on the same row and more than 2 cells to the right
-          if (index > action.index + 2 && index % GRID_WIDTH !== 0) {
-            // It's not the player and it's not nearby
-            return Object.assign({}, cell, {
-              player: false,
-              nearby: false
-            });
-          }
-          /*** LEFT ***/
-          // If index is one or two cells lower and not on another line
-          if (
-            (index === action.index - 1 && index % GRID_WIDTH !== GRID_WIDTH - 1) ||
-            (index === action.index - 2 &&
-              index % GRID_WIDTH !== GRID_WIDTH - 1 &&
-              index % GRID_WIDTH !== GRID_WIDTH - 2)
-          ) {
-            // It is nearby and explored
-            return Object.assign({}, cell, {
-              player: false,
-              nearby: true,
-              explored: true
-            });
-          }
-          // If it's on the same row and more than 2 cells to the left
-          if (
-            index < action.index - 2 &&
-            index % GRID_WIDTH !== GRID_WIDTH - 1 &&
-            index % GRID_WIDTH !== GRID_WIDTH - 2
-          ) {
-            // It's not the player and it's not nearby
-            return Object.assign({}, cell, {
-              player: false,
-              nearby: false
-            });
-          }
-          // Otherwise, the cell is not the player
+          // simply say so
           return Object.assign({}, cell, {
             player: false
           });
         }
-        // if one or two cells lower index, exists, and not on another line
-        // if one GRID_WIDTH cells higher index, +/- 1, exists, and not past the edge
-        // if one GRID_WIDTH cells lower index, +/- 1, exists, and not past the edge
+        // Fallback - should not be reached
         return cell;
       });
     // Move player one cell right
@@ -110,7 +60,7 @@ const grid = (state = gridData, action) => {
     case t.MOVE_SOUTH:
       return state.map((cell, index) => {
         // Deactivate the current cell if the cell below exists
-        if (index === action.position && index + GRID_WIDTH <= TOTAL_CELLS) {
+        if (index === action.position && index + GRID_WIDTH < TOTAL_CELLS) {
           return Object.assign({}, cell, {
             player: false
           });
