775dbaef463d1222415d818c3e4a1699f22236b5
CharmedSatyr <z@charmed.tech>
2018-04-05 08:45:32 +0900

    Grid.js was not primarily presentational; merged it entirely into GridContainer.js

--- src/containers/GridContainer.js
+++ src/containers/GridContainer.js
@@ -1,6 +1,76 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Grid from '../components/Grid.js';
 import { moveEast, moveSouth, moveNorth, moveWest, setCoordinates } from '../actions/movement';
+
+import Cell from '../components/Cell';
+import './Grid.css';
+
+class Grid extends Component {
+  constructor(props) {
+    super(props);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  handleKeyPress(e) {
+    const { coordinates, moveEast, moveSouth, moveWest, moveNorth } = this.props;
+    switch (e.keyCode) {
+      // North
+      case 38:
+      case 87:
+        moveNorth(coordinates);
+        break;
+      // East
+      case 39:
+      case 68:
+        moveEast(coordinates);
+        break;
+      // South
+      case 40:
+      case 83:
+        moveSouth(coordinates);
+        break;
+      // West
+      case 37:
+      case 65:
+        moveWest(coordinates);
+        break;
+      case 32:
+        console.log('spacebar');
+        break;
+      default:
+        return;
+    }
+  }
+  componentWillMount() {
+    window.addEventListener('keydown', e => this.handleKeyPress(e));
+
+    // Initialize the starting cell from state
+    const { coordinates, setCoordinates } = this.props;
+    setCoordinates(coordinates);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', e => this.handleKeyPress(e));
+  }
+  render() {
+    const { gridData, setCoordinates } = this.props;
+
+    const cells = gridData
+      // Create an array of Cells containing data from the store
+      .map((item, index) => (
+        <Cell
+          coordinates={item.coordinates}
+          fn={() => {
+            setCoordinates(item.coordinates); // Testing function
+          }}
+          key={index}
+          player={item.player}
+        />
+      ));
+
+    // Render the cells array
+    return <div className="Grid">{cells}</div>;
+  }
+}
 
 const mapStateToProps = state => ({
   gridData: state.grid,
