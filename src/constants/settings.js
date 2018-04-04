dd12b4f37b92ac64d45e819fa8ac1052595501d2
CharmedSatyr <z@charmed.tech>
2018-04-05 08:46:34 +0900

    enlarged grid, restructured calculations

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -1,3 +1,3 @@
-export const TOTAL_CELLS = 100;
-export const GRID_HEIGHT = 10;
-export const GRID_WIDTH = TOTAL_CELLS / GRID_HEIGHT;
+export const GRID_HEIGHT = 20;
+export const GRID_WIDTH = 30; // TOTAL_CELLS / GRID_HEIGHT;
+export const TOTAL_CELLS = GRID_HEIGHT * GRID_WIDTH;
