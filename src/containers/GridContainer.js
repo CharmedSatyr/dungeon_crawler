4c3ea77b350e50af63e330bebc8570fd81f38342
CharmedSatyr <z@charmed.tech>
2018-04-09 21:08:22 +0900

    +preventDefault, use JS grid width

--- src/containers/GridContainer.js
+++ src/containers/GridContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { moveEast, moveSouth, moveNorth, moveWest, setCoordinates } from '../actions/movement';
+import { GRID_WIDTH } from '../constants/settings';
 
 import Cell from '../components/Cell';
 import './Grid.css';
@@ -17,21 +18,25 @@ class Grid extends Component {
       // North
       case 38:
       case 87:
+        e.preventDefault();
         moveNorth(coordinates);
         break;
       // East
       case 39:
       case 68:
+        e.preventDefault();
         moveEast(coordinates);
         break;
       // South
       case 40:
       case 83:
+        e.preventDefault();
         moveSouth(coordinates);
         break;
       // West
       case 37:
       case 65:
+        e.preventDefault();
         moveWest(coordinates);
         break;
       case 32:
@@ -68,7 +73,12 @@ class Grid extends Component {
       ));
 
     // Render the cells array
-    return <div className="Grid">{cells}</div>;
+    return (
+      // Grid width must be Cell width * GRID_WIDTH for columns to line up
+      <div className="Grid" style={{ width: 40 * GRID_WIDTH }}>
+        {cells}
+      </div>
+    );
   }
 }
 
