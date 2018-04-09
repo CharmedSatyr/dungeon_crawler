71341a6274940f1cb412c917c0dd5933b8a7894d
CharmedSatyr <z@charmed.tech>
2018-04-09 12:00:28 +0900

    +seed room top corner coordinates are now dynamic; +partial function to add room somewhere north of the seed

--- src/reducers/grid.js
+++ src/reducers/grid.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import * as c from '../constants/settings';
 import * as t from '../constants/action-types';
 
-// 1. Create an empty grid
+// 1. Create an empty grid with the desired keys
 let gridData = [];
 
 for (let i = 0; i < c.GRID_HEIGHT; i++) {
@@ -13,7 +13,7 @@ for (let i = 0; i < c.GRID_HEIGHT; i++) {
     gridData.push({
       coordinates: { x, y },
       player: false, // designated player character
-      opacity: _.random(0.4, 0.9), // Opacity for styling
+      opacity: 1, // _.random(0.4, 0.9), // Opacity for styling
       type: 0
     });
   }
@@ -24,23 +24,58 @@ const [min, max] = c.ROOM_SIZE_RANGE;
 console.log('min, max:', '[' + min + ', ' + max + ']');
 
 const firstRoom = {
-  // x: _.random(1, c.GRID_WIDTH - max),
-  // y: _.random(1, c.GRID_HEIGHT - max),
+  x: _.random(1, c.GRID_WIDTH - max), // x top corner placement
+  y: _.random(1, c.GRID_HEIGHT - max), // y top corner placement
   height: _.random(min, max), // height of first room
   width: _.random(min, max) // width of first room
 };
 console.log('firstRoom Dimensions: ', '[' + firstRoom.height + ', ' + firstRoom.width + ']');
+console.log('firstRoom top corner: ', '[' + firstRoom.x + ', ' + firstRoom.y + ']');
 
 // 3. place the first room onto the grid
-const placeCells = (gridData, { height = 1, width = 1 }, type = 'floor') => {
+const placeCells = (gridData, { x = 0, y = 0, height = 1, width = 1 }, type = 'floor') => {
   for (let i in gridData) {
-    if (gridData[i].coordinates.x <= height && gridData[i].coordinates.y <= width) {
+    if (
+      gridData[i].coordinates.x >= x &&
+      gridData[i].coordinates.x < x + width &&
+      gridData[i].coordinates.y >= y &&
+      gridData[i].coordinates.y < y + height
+    ) {
       gridData[i].type = type;
     }
   }
   return gridData;
 };
 gridData = placeCells(gridData, firstRoom);
+
+// 4. place additional rooms based on that seed.
+const createRoomsFromSeed = (gridData, { x, y, height, width }, range = c.ROOM_SIZE_RANGE) => {
+  const [min, max] = range;
+
+  // Generated room values for each edge of the seed room
+  const roomValues = [];
+
+  // Make rooms north of the seed
+  const north = { height: _.random(min, max), width: _.random(min, max) };
+  north.x = _.random(x, x + width - 1);
+  north.y = y - north.height - 1;
+  north.door = {};
+  north.door.x = _.random(north.x, Math.min(north.x + north.width + x + width) - 1);
+  north.door.y = y - 1;
+  roomValues.push(north);
+
+  const placedRooms = [];
+  roomValues.forEach(room => {
+    // place room
+    gridData = placeCells(gridData, room);
+    // place door
+    gridData = placeCells(gridData, { x: room.door.x, y: room.door.y }, 'floor');
+    // ned placed room values for the next seeds
+    placedRooms.push(room);
+  });
+  console.log('createRoomsFromSeed output: ', { gridData, placedRooms });
+};
+createRoomsFromSeed(gridData, firstRoom);
 
 // Grid reducers
 const grid = (state = gridData, action) => {
@@ -50,8 +85,7 @@ const grid = (state = gridData, action) => {
         // Set cell as player
         if (cell.coordinates.x === action.x && cell.coordinates.y === action.y) {
           return Object.assign({}, cell, {
-            player: true,
-            opacity: 1
+            player: true
           });
           // Only one player at a time
         } else {
