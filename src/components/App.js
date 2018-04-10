367d007c0ed716694e27ea93c441efaafa0fa943
CharmedSatyr <z@charmed.tech>
2018-04-10 10:21:15 +0900

    renamed GridContainer => Grid

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Grid from '../containers/GridContainer';
+import Grid from '../containers/Grid';
 
 const App = ({ decrement, increment, value }) => {
   return (
