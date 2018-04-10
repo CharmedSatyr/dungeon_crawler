9058f5f51812ea10d59e0f5979ca0ea787b419d0
CharmedSatyr <z@charmed.tech>
2018-04-10 11:16:07 +0900

    +tweaks to allow hallways and 15 rooms

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -1,5 +1,5 @@
 export const GRID_HEIGHT = 20;
 export const GRID_WIDTH = 20; // TOTAL_CELLS / GRID_HEIGHT;
 export const TOTAL_CELLS = GRID_HEIGHT * GRID_WIDTH;
-export const ROOM_SIZE_RANGE = [2, 8]; // [min, max]
-export const MAX_ROOMS = 3;
+export const ROOM_SIZE_RANGE = [1, 8]; // [min, max]
+export const MAX_ROOMS = 15;
