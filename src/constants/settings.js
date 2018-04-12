4cf3050d314087b7871387ff60f266471bfae1f7
CharmedSatyr <z@charmed.tech>
2018-04-12 10:44:53 +0900

    +max rooms to 25 from 15

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -3,4 +3,4 @@ export const GRID_HEIGHT = 20; // Number of cells
 export const GRID_WIDTH = 30; // Number of cells
 export const TOTAL_CELLS = GRID_HEIGHT * GRID_WIDTH;
 export const ROOM_SIZE_RANGE = [2, 8]; // [min, max]
-export const MAX_ROOMS = 15;
+export const MAX_ROOMS = 25;
