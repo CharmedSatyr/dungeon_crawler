d09982bda928fc38dca0c1bc566026d07bd34e2e
CharmedSatyr <z@charmed.tech>
2018-04-15 19:52:17 +0900

    +code for character sprite to represent Player

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import * as c from '../../constants/settings';
+import sprite from './heroine-sprite.png';
 
-const style = { height: c.CELL_SIDE, width: c.CELL_SIDE };
-const Player = () => (
-  <div className="Cell player" style={style}>
-    P
-  </div>
+const Player = ({ coordinates }) => (
+  <div
+    style={{
+      backgroundImage: `url('${sprite}')`,
+      backgroundPosition: `0px ${c.SPRITE_SIZE * 1}px`,
+      left:
+        Math.floor(
+          (document.documentElement.clientWidth - c.CELL_SIDE * c.GRID_WIDTH) / 2 +
+            c.CELL_SIDE * coordinates.x
+        ) - 3.5, // left is cell side * x coordinate, counting from the start of the grid not the browser window
+      top: 195 + c.CELL_SIDE * coordinates.y,
+      height: 70,
+      overflow: 'hidden',
+      position: 'absolute',
+      width: 70
+    }}
+  />
 );
 
 export default Player;
