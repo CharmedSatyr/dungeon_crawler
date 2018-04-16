2d4bc787a1b6416a9a56532954aae1a1fd50d2d2
CharmedSatyr <z@charmed.tech>
2018-04-16 11:17:35 +0900

    split redux store into separate file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 import './index.css';
 import App from './components/App';
 
 import registerServiceWorker from './registerServiceWorker';
-
-import { Provider } from 'react-redux';
-
-import { createStore } from 'redux';
-import reducer from './reducers/index';
-const store = createStore(reducer);
-
-console.log('Initial state:', store.getState());
 
 render(
   <Provider store={store}>
