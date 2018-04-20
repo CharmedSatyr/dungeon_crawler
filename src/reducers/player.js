33aed32e56c883fef4bce8b42ce653442744fc67
CharmedSatyr <z@charmed.tech>
2018-04-20 22:34:31 +0900

    testing tweaks

--- src/reducers/player.js
+++ src/reducers/player.js
@@ -1,11 +1,18 @@
+import _ from 'lodash';
+import * as t from '../constants/action-types';
+
 const initialState = {
+  level: 1,
   health: 20,
-  level: 1
+  damage: _.random(1, 20)
 };
 
 // Player reducers
 const player = (state = initialState, action) => {
   switch (action.type) {
+    case t.ATTACK:
+      console.log('ATTACK!');
+      return state;
     default:
       return state;
   }
