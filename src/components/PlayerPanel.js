fafee93483580f1dd2a28648cd4713aac689a80b
CharmedSatyr <z@charmed.tech>
2018-04-21 10:14:36 +0900

    +styling and restructuring to reflect new player object properties

--- src/components/PlayerPanel.js
+++ src/components/PlayerPanel.js
@@ -2,10 +2,28 @@ import React from 'react';
 
 const PlayerPanel = ({ stats }) => {
   return (
-    <div style={{ backgroundColor: 'yellow' }}>
+    <div
+      style={{
+        backgroundColor: 'yellow',
+        display: 'flex',
+        justifyContent: 'flex-start',
+        flexDirection: 'column'
+      }}
+    >
       <h3>Midarin Do'ana</h3>
-      <span>Level: {stats.level}</span>
-      <span>Health: {stats.health} </span>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          marginTop: -20,
+          width: '100%'
+        }}
+      >
+        <p>Level: {stats.level}</p>
+        <p>Health: {stats.health} </p>
+        <p>Weapon: {stats.weapon.name}</p>
+        <p>Damage: {`${stats.weapon.min_damage} - ${stats.weapon.max_damage}`}</p>
+      </div>
     </div>
   );
 };
