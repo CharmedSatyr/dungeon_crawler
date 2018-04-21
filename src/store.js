117198e6b5d1f13593d8f0e991c8ca07312bac58
CharmedSatyr <z@charmed.tech>
2018-04-21 17:41:12 +0900

    +redux-batched-actions middleware install and setup

--- src/store.js
+++ src/store.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
+import { enableBatching, batchDispatchMiddleware } from 'redux-batched-actions';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import reducer from './reducers/index';
 
-const middlewares = [thunk];
+const middlewares = [thunk, batchDispatchMiddleware];
 
 if (process.env.NODE_ENV === `development`) {
   middlewares.push(logger);
 }
 
-export const store = createStore(reducer, applyMiddleware(...middlewares));
-export const getState = store.getState;
+export const store = createStore(enableBatching(reducer, applyMiddleware(...middlewares)));
+export const { getState } = store;
