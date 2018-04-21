1662b6d3da75e9bedc88907f258629385294362d
CharmedSatyr <z@charmed.tech>
2018-04-21 17:42:08 +0900

    +dedicated messaging reducer; -attack messages

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,13 +1,13 @@
 import * as t from '../constants/action-types';
 
 const initialState = [
-  "Prince Few got drunk and was kidnapped. It's your duty to save him because...\n Well, nobody else wants to. Busy, you see."
+  "Prince Few got drunk and was kidnapped by orcs. It's your duty to save him because... Well, nobody else wants to. Busy, you see."
 ];
 
 // Player reducers
 const messages = (state = initialState, action) => {
   switch (action.type) {
-    case t.ATTACK:
+    case t.MESSAGE:
       const m = state;
       m.push(action.message);
       if (m.length > 3) {
