b44510ffff311f3906d4e184e4dcea3aad45549c
CharmedSatyr <z@charmed.tech>
2018-04-22 16:21:48 +0900

    Loot should no longer block doors

--- src/reducers/grid/populate.js
+++ src/reducers/grid/populate.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import * as c from '../../constants/settings';
 import tileTypes from '../../constants/tile-types';
 
-// Populate the data
+// Populate the data with payloads (player, enemies, etc.)
 export const populate = (data, level) => {
   // Add enemies
   const addEnemies = data => {
@@ -44,14 +44,132 @@ export const populate = (data, level) => {
 
   // Add loot
   const addLoot = data => {
-    for (let i in data) {
+    for (let i = 0; i < data.length; i++) {
       // Loot
-      const loot = { type: 'health' };
+      const setLoot = chances => {
+        switch (true) {
+          case chances <= 0.1:
+            return 'spear';
+          case chances <= 0.2:
+            return '2';
+          case chances <= 0.3:
+            return '3';
+          case chances <= 0.4:
+            return '4';
+          case chances <= 0.5:
+            return '5';
+          case chances <= 0.6:
+            return '6';
+          case chances <= 0.7:
+            return '7';
+          case chances <= 0.8:
+            return '8';
+          case chances <= 0.9:
+            return '9';
+          case chances <= 1.0:
+            return '10';
+          default:
+            return 'spear';
+        }
+      };
 
+      // Don't block a door with loot
+      const clearTheDoor = (data, i) => {
+        const barrier = tileTypes(level);
+        const path = tileTypes(level, 'path');
+
+        // No door to the right
+        const right = data[i + 1];
+        const upRight = data[i + 1 - c.GRID_WIDTH];
+        const downRight = data[i + 1 + c.GRID_WIDTH];
+
+        if (
+          upRight &&
+          right &&
+          downRight &&
+          upRight.type === barrier &&
+          right.type === path &&
+          downRight.type === barrier
+        ) {
+          return false;
+        }
+
+        // No door above
+        const up = data[i - c.GRID_WIDTH];
+        const upLeft = data[i - 1 - c.GRID_WIDTH];
+        if (
+          upLeft &&
+          up &&
+          upRight &&
+          upLeft.type === barrier &&
+          up.type === path &&
+          upRight.type === barrier
+        ) {
+          return false;
+        }
+
+        // No door left
+        const left = data[i - 1];
+        const downLeft = data[i - 1 + c.GRID_WIDTH];
+        if (
+          upLeft &&
+          left &&
+          downLeft &&
+          upLeft.type === barrier &&
+          left.type === path &&
+          downLeft.type === barrier
+        ) {
+          return false;
+        }
+
+        // No door below
+        const down = data[i + c.GRID_WIDTH];
+        if (
+          downLeft &&
+          down &&
+          downRight &&
+          downLeft.type === barrier &&
+          down.type === path &&
+          downRight.type === barrier
+        ) {
+          return false;
+        }
+
+        // Not in a doorway
+        if (
+          up &&
+          down &&
+          left &&
+          right &&
+          up.type !== path &&
+          down.type !== path &&
+          left.type === path &&
+          right.type === path
+        ) {
+          return false;
+        }
+
+        if (
+          up &&
+          down &&
+          left &&
+          right &&
+          up.type === path &&
+          down.type === path &&
+          left.type === barrier &&
+          right.type === barrier
+        ) {
+          return false;
+        }
+        return true;
+      };
+
+      const loot = { type: setLoot(Math.random()) };
       // 1% chance of a cell being occupied by loot
       if (
         Object.keys(data[i].payload).length === 0 &&
         data[i].type === tileTypes(level, 'path') &&
+        clearTheDoor(data, i) &&
         Math.random() > 0.99
       ) {
         data[i].payload = { loot };
