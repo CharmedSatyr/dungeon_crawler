e541f081559aa5775c59aa0f54946fc0b44db8d9
CharmedSatyr <z@charmed.tech>
2018-04-23 13:22:54 +0900

    removing redux-logger for now because it stopped working (due to redux-batched-actions perhaps)

--- src/store.js
+++ src/store.js
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import { enableBatching, batchDispatchMiddleware } from 'redux-batched-actions';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import reducer from './reducers/index';
 
 const middlewares = [thunk, batchDispatchMiddleware];
 
-if (process.env.NODE_ENV === `development`) {
-  middlewares.push(logger);
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   middlewares.push(logger);
+// }
 
 export const store = createStore(enableBatching(reducer, applyMiddleware(...middlewares)));
 export const { getState } = store;
