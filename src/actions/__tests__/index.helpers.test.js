ef7293a2bd57a7ad3cef57fe23fddbbdb732e6cc
CharmedSatyr <z@charmed.tech>
2018-04-26 06:38:46 +0900

    +additional action tests

--- src/actions/__tests__/index.helpers.test.js
+++ src/actions/__tests__/index.helpers.test.js
@@ -1,28 +1,47 @@
-import { getTargetPosition } from '../index.helpers';
+import * as h from '../index.helpers';
 import { GRID_WIDTH } from '../../constants/settings';
 
 describe('getTargetPosition', () => {
   it('should return the coordinates and index of the cell the source (usually player) is moving to', () => {
     const originPosition = { coordinates: { x: 0, y: 0 }, index: 0 };
 
-    expect(getTargetPosition(originPosition, 'north')).toEqual({
+    expect(h.getTargetPosition(originPosition, 'north')).toEqual({
       coordinates: { x: 0, y: -1 },
       index: -GRID_WIDTH
     });
 
-    expect(getTargetPosition(originPosition, 'east')).toEqual({
+    expect(h.getTargetPosition(originPosition, 'east')).toEqual({
       coordinates: { x: 1, y: 0 },
       index: 1
     });
 
-    expect(getTargetPosition(originPosition, 'south')).toEqual({
+    expect(h.getTargetPosition(originPosition, 'south')).toEqual({
       coordinates: { x: 0, y: 1 },
       index: GRID_WIDTH
     });
 
-    expect(getTargetPosition(originPosition, 'west')).toEqual({
+    expect(h.getTargetPosition(originPosition, 'west')).toEqual({
       coordinates: { x: -1, y: 0 },
       index: -1
     });
   });
 });
+
+describe('playerAdjacentPositions', () => {
+  it('should return an array of coordinate/index objects for cells adjacent to player in order east, north, south, west', () => {
+    const playerPosition = { coordinates: { x: 0, y: 0 }, index: 0 };
+    const e = { coordinates: { x: 1, y: 0 }, index: 1 };
+    const n = { coordinates: { x: 0, y: -1 }, index: -GRID_WIDTH };
+    const s = { coordinates: { x: 0, y: 1 }, index: GRID_WIDTH };
+    const w = { coordinates: { x: -1, y: 0 }, index: -1 };
+    expect(h.playerAdjacentPositions(playerPosition)).toEqual([e, n, s, w]);
+  });
+});
+//
+//describe('facePlayer', () => {
+//  it('should return direction entity should face to face player if position is adjacent to player, else null', () => {
+//    const
+//    const playerPosition = { coordinates: { x: 0, y: 0 }, index: 0 };
+//  })
+//})
+//
---
+++ src/actions/__tests__/index.test.js
@@ -0,0 +1,101 @@
+import * as a from '../index';
+import * as t from '../../constants/action-types';
+
+/*** SIMPLE ACTION CREATORS ***/
+describe('add_xp action creator', () => {
+  it('should return an action to add experience to the player', () => {
+    const args = 56;
+    const action = { type: t.ADD_XP, amount: 56 };
+    expect(a.add_xp(args)).toEqual(action);
+  });
+});
+
+describe('attack action creator', () => {
+  it('should return an action to attack an object in a direction', () => {
+    const args = ['north', { a: 0 }, { b: 1 }];
+    const action = {
+      type: t.ATTACK,
+      direction: args[0],
+      targetPosition: args[1],
+      targetObj: args[2]
+    };
+
+    expect(a.attack(...args)).toEqual(action);
+  });
+});
+
+describe('facing action creator', () => {
+  it('should return an action to set the direction an entity is facing', () => {
+    const args = ['north', { x: 0, y: 0 }];
+    const action = { type: t.FACING, direction: args[0], entityPosition: args[1] };
+    expect(a.facing(...args)).toEqual(action);
+  });
+});
+
+describe('go action creator', () => {
+  it('should return an action to move an entity in a direction', () => {
+    const args = ['north', { x: 0, y: 0 }, { type: 'target' }];
+    const action = {
+      type: t.MOVE,
+      direction: args[0],
+      targetPosition: args[1],
+      targetObj: args[2]
+    };
+    expect(a.go(...args)).toEqual(action);
+  });
+});
+
+describe('level_up action creator', () => {
+  it('should return an action to level up the player', () => {
+    const action = { type: t.LEVEL_UP };
+    expect(a.level_up()).toEqual(action);
+  });
+});
+
+describe('message action creator', () => {
+  it('should return an action to display a message', () => {
+    const args = 'test!';
+    const action = {
+      type: t.MESSAGE,
+      msg: args
+    };
+    expect(a.message(args)).toEqual(action);
+  });
+});
+
+describe('next_level action creator', () => {
+  it('should return an action to advance the player to the next game level', () => {
+    const action = { type: t.NEXT_LEVEL };
+    expect(a.next_level()).toEqual(action);
+  });
+});
+
+describe('open action creator', () => {
+  it('should return an action to open a door or other target object', () => {
+    const args = ['north', { x: 0, y: 0 }, { type: 'target' }];
+    const action = {
+      type: t.OPEN,
+      direction: args[0],
+      targetPosition: args[1],
+      targetObj: args[2]
+    };
+    expect(a.open(...args)).toEqual(action);
+  });
+});
+
+describe('take_damage action creator', () => {
+  it('should return an action to cause the player to take a specified amount of damage', () => {
+    const args = 12;
+    const action = { type: t.TAKE_DAMAGE, damage: 12 };
+    expect(a.take_damage(args)).toEqual(action);
+  });
+});
+
+/*** THUNKS ***/
+describe('hostile_enemies action creator thunk', () => {
+  it('should trigger `message`, `take_damage`, and `facing` action creators if an enemy is adjacent to the player', () => {
+    //const obj1
+    // const actions = [obj1, obj2, obj3]
+    expect(a.hostile_enemies()).toEqual();
+  });
+});
