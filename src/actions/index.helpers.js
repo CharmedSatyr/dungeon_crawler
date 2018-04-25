06d29e8ce69b06a1e82a293d677a81bf387fdd26
CharmedSatyr <z@charmed.tech>
2018-04-26 06:38:19 +0900

    +preliminary additional function exports

--- src/actions/index.helpers.js
+++ src/actions/index.helpers.js
@@ -27,3 +27,30 @@ export const getTargetPosition = (originPosition, direction) => {
       return originPosition;
   }
 };
+
+// Return positions of cells adjacent to player
+export const playerAdjacentPositions = playerPosition => {
+  const e = getTargetPosition(playerPosition, 'east');
+  const n = getTargetPosition(playerPosition, 'north');
+  const s = getTargetPosition(playerPosition, 'south');
+  const w = getTargetPosition(playerPosition, 'west');
+
+  return [e, n, s, w];
+};
+
+// Return direction entity should be facing
+// to face player if position is adjacent to player
+export const facePlayer = (position, playerPosition) => {
+  switch (position) {
+    case playerAdjacentPositions(playerPosition)[0]:
+      return 'west';
+    case playerAdjacentPositions(playerPosition)[1]:
+      return 'south';
+    case playerAdjacentPositions(playerPosition)[2]:
+      return 'north';
+    case playerAdjacentPositions(playerPosition)[3]:
+      return 'east';
+    default:
+      return null;
+  }
+};
