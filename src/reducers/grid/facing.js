9bdd9e465e215ab5a8465dc70a32918c929cd11e
CharmedSatyr <z@charmed.tech>
2018-04-28 20:42:00 +0900

    rewrote facing reducer to explicitly take a flag when it's being used by an enemy and provide overall usage clarity

--- src/reducers/grid/facing.js
+++ src/reducers/grid/facing.js
@@ -1,25 +1,30 @@
 import _ from 'lodash';
 import * as h from '../../actions/index.helpers';
 
-// Changes the direction the player is facing
-// Independent from movement so the player can face different directions even if no movement is possible
-export const facing = (data, playerPosition, targetObj) => {
-  // Essentially just splice a single new value (direction) into the current player object
-  let newData = _.clone(data);
-  const direction = h.getDirection(targetObj, playerPosition);
-
-  // Don't overwrite other payloads when updating the object
-  const updateObj = Object.assign({}, targetObj);
-
-  if (updateObj.payload.player) {
-    updateObj.payload.player.facing = direction;
+// Changes the direction entities are facing
+// Independent from movement so the player can face different directions even if no movement is possible--
+// e.g., in corridors or when attacking
+export const facing = (data, playerPosition, targetObj, flag) => {
+  const newData = _.clone(data);
+  // Supposing there is no flag
+  if (!flag) {
+    // Update the `facing` value of the current player object with inferred travel direction
+    const direction = h.getDirection(playerPosition, targetObj);
+    const player = data[playerPosition.index];
+    const newObj = Object.assign({}, player);
+    newObj.payload.player.facing = direction;
+    newData.splice(playerPosition.index, 1, newObj);
   }
 
-  if (updateObj.payload.enemy) {
-    updateObj.payload.enemy.facing = direction;
+  // With a flag, the function has been called by hostile_enemies and means the enemies should face the player
+  // The order of things is modified
+  if (flag) {
+    const direction = h.getDirection(targetObj, playerPosition);
+    const enemy = targetObj;
+    const newObj = Object.assign({}, enemy);
+    newObj.payload.enemy.facing = direction;
+    newData.splice(enemy.index, 1, newObj);
   }
-
-  newData.splice(targetObj.index, 1, updateObj);
 
   return newData;
 };
