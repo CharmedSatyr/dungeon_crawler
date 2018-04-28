05a7842308c5152430311b94fd4be093d8cb5eff
CharmedSatyr <z@charmed.tech>
2018-04-28 20:36:32 +0900

    +comment, spacing tweaks

--- src/reducers/grid/move.js
+++ src/reducers/grid/move.js
@@ -9,17 +9,16 @@ export const move = (data, playerPosition, targetObj) => {
   let newData = _.clone(data);
 
   // The player is no longer at its current position
-  // Don't overwrite other payloads when updating the object
   const currentPlayerObj = data[playerPosition.index];
   const formerObj = Object.assign({}, currentPlayerObj);
   delete formerObj.payload.player;
   newData.splice(playerPosition.index, 1, formerObj);
 
   // The player is at the next position
-  // Don't overwrite other payloads when updating the object
   const newObj = Object.assign({}, targetObj);
-  const direction = h.getDirection(playerPosition, targetObj);
 
+  // Don't overwrite other payloads when updating the object
+  const direction = h.getDirection(playerPosition, targetObj);
   newObj.payload
     ? (newObj.payload.player = { facing: direction })
     : (newObj.payload = { player: { facing: direction } });
