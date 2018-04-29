4c61e19d71ecb3850f0bacaa37e9236557f3f241
CharmedSatyr <z@charmed.tech>
2018-04-29 22:11:27 +0900

    complete tests for messages reducer

--- src/reducers/__tests__/messages.test.js
+++ src/reducers/__tests__/messages.test.js
@@ -4,10 +4,21 @@ import * as t from '../../constants/action-types';
 describe('`messages` reducer', () => {
   it('should return an array of strings', () => {
     const initialState = ['str'];
-    const action = { type: t.MESSAGE, message: 'msg' };
+    const action = { type: t.MESSAGE, msg: 'msg' };
     expect(messages(initialState, action)).toEqual(expect.arrayContaining(initialState));
   });
 
-  it('should update state to contain new `action.message` input');
-  it('should never be more than three items long');
+  it('should update state to contain new `action.message` input', () => {
+    const initialState = ['str'];
+    const action = { type: t.MESSAGE, msg: 'msg' };
+    const result = ['str', 'msg'];
+    expect(messages(initialState, action)).toEqual(result);
+  });
+
+  it('should contain only the newest three messages', () => {
+    const initialState = ['str', 'a', 'b'];
+    const action = { type: t.MESSAGE, msg: 'c' };
+    const result = ['a', 'b', 'c'];
+    expect(messages(initialState, action)).toEqual(result);
+  });
 });
