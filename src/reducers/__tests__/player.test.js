ebc1a3d06b14b72a932884c6bbe74733e934d648
CharmedSatyr <z@charmed.tech>
2018-04-30 08:28:23 +0900

    +testing notes for player drink function

--- src/reducers/__tests__/player.test.js
+++ src/reducers/__tests__/player.test.js
@@ -1,3 +1,10 @@
 describe('`player` reducer', () => {
-  it('should heal the player for a variable number of points when a `drink` action is received');
+  it(
+    'should add an action.amount of experience to state.experience when an `ADD_XP` action is received'
+  );
+  it(
+    'should raise the player state.level by 1 and the player state.health.max by a calculated amount when a `LEVEL_UP` action is received'
+  );
+  it('should heal the player for an action.amount of points when a `DRINK` action is received');
+  it('should subtract action.damage health from state.health.current, with 0 as the floor');
 });
