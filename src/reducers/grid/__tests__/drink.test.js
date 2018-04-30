5782be9ad2a60ffc62ddc53b6443748bdb879069
CharmedSatyr <z@charmed.tech>
2018-05-01 08:29:22 +0900

    +drink reducer function test

---
+++ src/reducers/grid/__tests__/drink.test.js
@@ -0,0 +1,11 @@
+import drink from '../drink';
+
+it('should set the targetObj payload to an empty barrel from a full one', () => {
+  const targetObj = { index: 0, payload: { loot: { barrel: { full: true } } } };
+  const data = [targetObj];
+
+  const updateObj = { index: 0, payload: { loot: { barrel: { full: false } } } };
+  const newData = [updateObj];
+
+  expect(drink(data, targetObj)).toEqual(newData);
+});
