b680d29c8949a8e78fd87af629c810b01b3760d1
CharmedSatyr <z@charmed.tech>
2018-05-05 14:57:20 +0900

    converted Player to Component

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,38 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import * as c from '../../constants/settings';
 import PropTypes from 'prop-types';
 
 import './styles.css';
 
-const faceDirection = facing => {
-  switch (facing) {
-    case 'north':
-      return -8;
-    case 'east':
-      return -11;
-    case 'south':
-      return -10;
-    case 'west':
-      return -9;
-    default:
-      return -10;
+export default class Player extends Component {
+  faceDirection(facing) {
+    switch (facing) {
+      case 'north':
+        return -8;
+      case 'west':
+        return -9;
+      case 'south':
+        return -10;
+      case 'east':
+        return -11;
+      default:
+        return -10;
+    }
   }
-};
-
-const Player = ({ animation, facing }) => (
-  <div
-    className={`sprite ${animation[animation.length - 1]}-${facing}`}
-    style={{
-      backgroundPosition: `0px ${c.SPRITE_SIZE * faceDirection(facing)}px`,
-      height: c.SPRITE_SIZE,
-      width: c.SPRITE_SIZE
-    }}
-  />
-);
+  render() {
+    const { faceDirection } = this;
+    const { animation, facing } = this.props;
+    let playerClass;
+    animation.length
+      ? (playerClass = `sprite ${animation[animation.length - 1]}-${facing}`)
+      : (playerClass = 'sprite');
+    return (
+      <div
+        className={playerClass}
+        style={{
+          backgroundPosition: `0px ${c.SPRITE_SIZE * faceDirection(facing)}px`,
+          height: c.SPRITE_SIZE,
+          width: c.SPRITE_SIZE
+        }}
+      />
+    );
+  }
+}
 
 Player.propTypes = {
   animation: PropTypes.arrayOf(PropTypes.string),
   facing: PropTypes.string
 };
-
-export default Player;
