6e4e1e0fe9dc6d8699a9f38ce455aa067daf3eec
CharmedSatyr <z@charmed.tech>
2018-05-10 11:39:32 +0900

    clarify setVariety conditions/output

--- src/components/Loot/index.js
+++ src/components/Loot/index.js
@@ -6,21 +6,24 @@ import tiles from './Loot.png';
 
 const emptyBarrel = '0px 0px';
 const waterBarrel = '-40px 0px';
+const warningSquare = '0px 40px';
 
 const setVariety = variety => {
-  if (variety.barrel.full) {
+  if (variety.barrel && variety.barrel.full) {
     return waterBarrel;
-  } else {
+  } else if (variety.barrel && !variety.barrel.full) {
     return emptyBarrel;
+  } else {
+    return warningSquare;
   }
 };
 
-const Loot = props => {
+const Loot = ({ variety }) => {
   return (
     <div
       style={{
         backgroundImage: `url(${tiles})`,
-        backgroundPosition: setVariety(props.variety),
+        backgroundPosition: setVariety(variety),
         height: c.CELL_SIDE,
         width: c.CELL_SIDE
       }}
