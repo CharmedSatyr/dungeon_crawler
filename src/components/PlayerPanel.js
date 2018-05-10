29ff9e22d3fbb4e4aea0beb2fcfabd11c4b2eacd
CharmedSatyr <z@charmed.tech>
2018-05-10 18:08:42 +0900

    simplified structure; no content changes

--- src/components/PlayerPanel.js
+++ src/components/PlayerPanel.js
@@ -2,41 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import * as g from '../constants/gameplay';
 
-const PlayerPanel = ({ stats }) => {
-  return (
+const PlayerPanel = ({ stats }) => (
+  <div
+    style={{
+      backgroundColor: 'yellow',
+      display: 'flex',
+      justifyContent: 'flex-start',
+      flexDirection: 'column'
+    }}
+  >
+    <h3>Midarin Do'ana</h3>
     <div
       style={{
-        backgroundColor: 'yellow',
         display: 'flex',
-        justifyContent: 'flex-start',
-        flexDirection: 'column'
+        justifyContent: 'space-evenly',
+        marginTop: -20,
+        width: '100%'
       }}
     >
-      <h3>Midarin Do'ana</h3>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-evenly',
-          marginTop: -20,
-          width: '100%'
-        }}
-      >
-        <p>Level: {stats.level}</p>
-        <p>Experience: {stats.experience}</p>
-        <p>
-          Health: {stats.health.current}/{stats.health.max}
-        </p>
-        <p>Weapon: {stats.weapon.name}</p>
-        <p>
-          Damage: {g.damageCalc(stats.weapon.min_damage, stats.weapon.min_damage, stats.level)}
-          -
-          {g.damageCalc(stats.weapon.max_damage, stats.weapon.max_damage, stats.level)}
-        </p>
-        <p>Gold: {stats.gold}</p>
-      </div>
+      <p>Level: {stats.level}</p>
+      <p>Experience: {stats.experience}</p>
+      <p>
+        Health: {stats.health.current}/{stats.health.max}
+      </p>
+      <p>Weapon: {stats.weapon.name}</p>
+      <p>
+        Damage: {g.damageCalc(stats.weapon.min_damage, stats.weapon.min_damage, stats.level)}
+        -
+        {g.damageCalc(stats.weapon.max_damage, stats.weapon.max_damage, stats.level)}
+      </p>
+      <p>Gold: {stats.gold}</p>
     </div>
-  );
-};
+  </div>
+);
 
 PlayerPanel.propTypes = {
   stats: PropTypes.object.isRequired
