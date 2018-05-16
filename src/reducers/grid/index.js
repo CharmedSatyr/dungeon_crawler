4c106e453c6674338ee1cd54459145069484210c
CharmedSatyr <z@charmed.tech>
2018-05-16 11:20:54 +0700

    generate only requires level arg now; dangling commas

--- src/reducers/grid/index.js
+++ src/reducers/grid/index.js
@@ -12,8 +12,8 @@ const initialState = {
   level: 0,
   playerPosition: {
     coordinates: { x: 0, y: 0 },
-    index: 0
-  }
+    index: 0,
+  },
 };
 
 // Grid reducers
@@ -21,30 +21,30 @@ const grid = (state = initialState, { damage, direction, flag, targetObj, type }
   switch (type) {
     case t.ATTACK:
       return Object.assign({}, state, {
-        data: attack(state.data, targetObj, damage)
+        data: attack(state.data, targetObj, damage),
       });
     case t.DRINK:
       return Object.assign({}, state, {
-        data: drink(state.data, targetObj)
+        data: drink(state.data, targetObj),
       });
     case t.FACING:
       return Object.assign({}, state, {
-        data: facing(state.data, state.playerPosition, targetObj, flag)
+        data: facing(state.data, state.playerPosition, targetObj, flag),
       });
     case t.MOVE:
       const updated = move(state.data, state.playerPosition, targetObj);
       return Object.assign({}, state, {
         data: updated.data,
-        playerPosition: updated.playerPosition
+        playerPosition: updated.playerPosition,
       });
     case t.NEXT_LEVEL:
       // New levels should be created on an empty data array
-      let level = populate(generate([], state.level + 1), state.level + 1);
+      let level = populate(generate(state.level + 1), state.level + 1);
       console.log('Entering level', state.level);
       return Object.assign({}, state, {
         data: level.data,
         level: state.level + 1,
-        playerPosition: level.playerPosition
+        playerPosition: level.playerPosition,
       });
     case t.OPEN:
       return Object.assign({}, state, { data: open(state.data, targetObj) });
