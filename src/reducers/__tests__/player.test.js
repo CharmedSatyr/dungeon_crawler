a437d12fa406b3622471bbadcb90a67fa7713732
CharmedSatyr <z@charmed.tech>
2018-05-18 15:42:27 +0700

    +ADD_ITEM and CHANGE_WEAPON reducers and tests; additional type tests

--- src/reducers/__tests__/player.test.js
+++ src/reducers/__tests__/player.test.js
@@ -1,43 +1,75 @@
 import player from '../player';
 import * as t from '../../constants/action-types';
 import * as g from '../../constants/gameplay';
+import * as l from '../../constants/loot';
 
 describe('`player` reducer', () => {
+  it('should have default state with `experience`, `gold`, `health`, `level`, `weapon`, and `inventory` properties', () => {
+    const action = { type: null };
+    expect(player(undefined, action)).toHaveProperty('experience');
+    expect(player(undefined, action)).toHaveProperty('gold');
+    expect(player(undefined, action)).toHaveProperty('health');
+    expect(player(undefined, action)).toHaveProperty('level');
+    expect(player(undefined, action)).toHaveProperty('weapon');
+    expect(player(undefined, action)).toHaveProperty('inventory');
+  });
+
+  it('should have default properties of the appropriate types', () => {
+    const action = { type: null };
+    expect(player(undefined, action)).toMatchObject({
+      experience: expect.any(Number),
+      gold: expect.any(Number),
+      health: expect.objectContaining({
+        current: expect.any(Number),
+        max: expect.any(Number),
+      }),
+      level: expect.any(Number),
+      weapon: expect.objectContaining({
+        name: expect.any(String),
+        min_damage: expect.any(Number),
+        max_damage: expect.any(Number),
+      }),
+      inventory: expect.arrayContaining([l.weapons.fists, l.weapons.dagger]),
+    });
+  });
+
+  it('should add an item to the inventory when `ADD_ITEM` action is received', () => {
+    const action = { type: t.ADD_ITEM, item: l.weapons.spear };
+    const inventory = [l.weapons.fists, l.weapons.dagger];
+    const state = { inventory };
+    const updatedInventory = [...inventory, l.weapons.spear];
+    const updatedState = { inventory: updatedInventory };
+    expect(player(state, action)).toEqual(updatedState);
+  });
+
+  it('should change the equipped weapon to the next in the inventory when `CHANGE_WEAPON` action is received', () => {
+    const action = { type: t.CHANGE_WEAPON };
+    const inventory = [l.weapons.fists, l.weapons.dagger];
+    const weapon = l.weapons.fists;
+    const state = { weapon, inventory };
+    const updatedState = { weapon: l.weapons.dagger, inventory };
+    expect(player(state, action)).toEqual(updatedState);
+  });
+
+  it('should change the equipped item to the first in the inventory if `CHANGE_WEAPON` action is received on the last inventory item', () => {
+    const action = { type: t.CHANGE_WEAPON };
+    const inventory = [l.weapons.fists, l.weapons.dagger];
+    const weapon = l.weapons.dagger;
+    const state = { weapon, inventory };
+    const updatedState = { weapon: l.weapons.fists, inventory };
+    expect(player(state, action)).toEqual(updatedState);
+  });
+
   it('should add an action.amount of experience to state.experience when an `ADD_XP` action is received', () => {
     const action = { type: t.ADD_XP, amount: 10 };
     const state0 = {
-      experience: 0
+      experience: 0,
     };
     const state1 = {
-      experience: 10
+      experience: 10,
     };
     const state2 = {
-      experience: 20
-    };
-
-    expect(player(state0, action)).toEqual(state1);
-    expect(player(state1, action)).toEqual(state2);
-  });
-
-  it('should raise the player state.level by 1 and the player state.health.max by a calculated amount when a `LEVEL_UP` action is received', () => {
-    const action = { type: t.LEVEL_UP };
-    const state0 = {
-      level: 1,
-      health: {
-        max: g.healthCalc(1)
-      }
-    };
-    const state1 = {
-      level: 2,
-      health: {
-        max: g.healthCalc(2)
-      }
-    };
-    const state2 = {
-      level: 3,
-      health: {
-        max: g.healthCalc(3)
-      }
+      experience: 20,
     };
 
     expect(player(state0, action)).toEqual(state1);
@@ -50,30 +82,55 @@ describe('`player` reducer', () => {
     const state0 = {
       health: {
         current: 1,
-        max: 10
-      }
+        max: 10,
+      },
     };
     const state1 = {
       health: {
         current: 10,
-        max: 10
-      }
+        max: 10,
+      },
     };
     // Standard health boost
     const state2 = {
       health: {
         current: 1,
-        max: 20
-      }
+        max: 20,
+      },
     };
     const state3 = {
       health: {
         current: 11,
-        max: 20
-      }
+        max: 20,
+      },
     };
     expect(player(state0, action)).toEqual(state1);
     expect(player(state2, action)).toEqual(state3);
+  });
+
+  it('should raise the player state.level by 1 and the player state.health.max by a calculated amount when a `LEVEL_UP` action is received', () => {
+    const action = { type: t.LEVEL_UP };
+    const state0 = {
+      level: 1,
+      health: {
+        max: g.healthCalc(1),
+      },
+    };
+    const state1 = {
+      level: 2,
+      health: {
+        max: g.healthCalc(2),
+      },
+    };
+    const state2 = {
+      level: 3,
+      health: {
+        max: g.healthCalc(3),
+      },
+    };
+
+    expect(player(state0, action)).toEqual(state1);
+    expect(player(state1, action)).toEqual(state2);
   });
 
   it('should subtract action.damage health from state.health.current, with 0 as the floor when a `TAKE_DAMAGE` action is received', () => {
@@ -81,24 +138,24 @@ describe('`player` reducer', () => {
     // Health doesn't go below 0
     const state0 = {
       health: {
-        current: 10
-      }
+        current: 10,
+      },
     };
     const state1 = {
       health: {
-        current: 0
-      }
+        current: 0,
+      },
     };
     // Standard health reduction
     const state2 = {
       health: {
-        current: 20
-      }
+        current: 20,
+      },
     };
     const state3 = {
       health: {
-        current: 5
-      }
+        current: 5,
+      },
     };
     expect(player(state0, action)).toEqual(state1);
     expect(player(state2, action)).toEqual(state3);
--- src/reducers/player.js
+++ src/reducers/player.js
@@ -1,13 +1,15 @@
 import * as t from '../constants/action-types';
 import * as g from '../constants/gameplay';
 import * as l from '../constants/loot';
+import _ from 'lodash';
 
 const initialState = {
   experience: 0,
   gold: 0,
   health: {},
   level: 1,
-  weapon: l.weapons.fists
+  weapon: l.weapons.fists,
+  inventory: [l.weapons.fists, l.weapons.dagger],
 };
 initialState.health.max = g.healthCalc(initialState.level);
 initialState.health.current = initialState.health.max;
@@ -15,17 +17,24 @@ initialState.health.current = initialState.health.max;
 // Player reducers
 const player = (state = initialState, action) => {
   switch (action.type) {
+    case t.ADD_ITEM:
+      const newItem = action.item;
+      const updatedInventory = _.clone(state.inventory);
+      updatedInventory.push(newItem);
+      return Object.assign({}, state, { inventory: updatedInventory });
     case t.ADD_XP:
       return Object.assign({}, state, { experience: state.experience + action.amount });
-    case t.LEVEL_UP:
-      const raisedLevel = state.level + 1;
-      const raisedHealth = g.healthCalc(raisedLevel);
-      return Object.assign(
-        {},
-        state,
-        { level: raisedLevel },
-        { health: { ...state.health, max: raisedHealth } }
-      );
+    case t.CHANGE_WEAPON:
+      const currentWeaponIndex = state.inventory.indexOf(state.weapon);
+      let nextWeapon;
+      if (state.inventory[currentWeaponIndex + 1]) {
+        nextWeapon = state.inventory[currentWeaponIndex + 1];
+      } else {
+        nextWeapon = state.inventory[0];
+      }
+      return Object.assign({}, state, {
+        weapon: nextWeapon,
+      });
     case t.DRINK:
       // Heal the player by the amount in the barrel if that won't put the player over max health
       // Otherwise, the player is back to max health
@@ -36,17 +45,26 @@ const player = (state = initialState, action) => {
           current:
             state.health.current + healthBoost <= state.health.max
               ? state.health.current + healthBoost
-              : state.health.max
-        }
+              : state.health.max,
+        },
       });
+    case t.LEVEL_UP:
+      const raisedLevel = state.level + 1;
+      const raisedHealth = g.healthCalc(raisedLevel);
+      return Object.assign(
+        {},
+        state,
+        { level: raisedLevel },
+        { health: { ...state.health, max: raisedHealth } }
+      );
     case t.TAKE_DAMAGE:
       // Show the remaining health unless the player is dead, then show 0
       const remaining = state.health.current - action.damage;
       return Object.assign({}, state, {
         health: {
           ...state.health,
-          current: remaining >= 0 ? remaining : 0
-        }
+          current: remaining >= 0 ? remaining : 0,
+        },
       });
     default:
       return state;
