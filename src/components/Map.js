7203ebb329d2bc156138a1401a5e2354a688bbcd
CharmedSatyr <z@charmed.tech>
2018-05-20 04:49:41 +0700

    formatting tweaks

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,13 +4,19 @@ import PropTypes from 'prop-types';
 
 // Map width must be Cell width * GRID_WIDTH for columns to line up
 const Map = ({ cells }) => (
-  <div style={{ display: 'flex', flexWrap: 'wrap', width: c.CELL_SIDE * c.GRID_WIDTH }}>
+  <div
+    style={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      width: c.CELL_SIDE * c.GRID_WIDTH,
+    }}
+  >
     {cells}
   </div>
 );
 
 Map.propTypes = {
-  cells: PropTypes.arrayOf(PropTypes.object).isRequired
+  cells: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default Map;
