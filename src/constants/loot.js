2d3d73b1b38afc2d06eb32ae52a0fd02d5874c5f
CharmedSatyr <z@charmed.tech>
2018-05-20 04:50:05 +0700

    +bad barrel constructor

--- src/constants/loot.js
+++ src/constants/loot.js
@@ -24,5 +24,12 @@ weapons.fists = new Weapon('Fists', 1, 3);
 weapons.dagger = new Weapon('Dagger', 3, 5);
 weapons.spear = new Weapon('Spear', 4, 9);
 
-export const fullBarrel = { barrel: { full: true } };
-export const emptyBarrel = { barrel: { full: false } };
+// Barrel constructor
+class Barrel {
+  constructor(full) {
+    this.barrel = { full };
+  }
+}
+
+export const fullBarrel = new Barrel(true);
+export const emptyBarrel = new Barrel(false);
