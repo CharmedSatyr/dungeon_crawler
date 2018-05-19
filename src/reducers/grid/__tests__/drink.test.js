1d94ffb833138bb850af7a53e0dc5eb71f196283
CharmedSatyr <z@charmed.tech>
2018-05-19 14:22:55 +0700

    +additional test

--- src/reducers/grid/__tests__/drink.test.js
+++ src/reducers/grid/__tests__/drink.test.js
@@ -1,13 +1,18 @@
 import drink from '../drink';
 
 describe('`drink` grid reducer function', () => {
+  const targetObj = { index: 0, payload: { loot: { barrel: { full: true } } } };
+  const updateObj = { index: 0, payload: { loot: { barrel: { full: false } } } };
   it('should set the targetObj payload to an empty barrel from a full one', () => {
-    const targetObj = { index: 0, payload: { loot: { barrel: { full: true } } } };
     const data = [targetObj];
-
-    const updateObj = { index: 0, payload: { loot: { barrel: { full: false } } } };
     const newData = [updateObj];
+    expect(drink(data, targetObj)).toEqual(newData);
+  });
 
+  it('should not change the payload of any other object', () => {
+    const bystanderObj = { index: 1, payload: { loot: { barrel: { full: true } } } };
+    const data = [targetObj, bystanderObj];
+    const newData = [updateObj, bystanderObj];
     expect(drink(data, targetObj)).toEqual(newData);
   });
 });
