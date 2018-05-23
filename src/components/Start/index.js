feae59a4fa8326d1e1d67dbbc03652f01b059221
CharmedSatyr <z@charmed.tech>
2018-05-23 21:28:23 +0700

    move start to its own folder; separate styles to scss

--- src/components/Start/index.js
+++ src/components/Start/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import * as c from '../constants/settings';
+import * as c from '../../constants/settings';
 import logo from './logo.svg';
 import heroine from './Heroine.png';
-import './Start.css';
+import few from './Few.png';
+import boss from './Boss.png';
 
 const Start = ({ fn }) => (
   <div
@@ -20,7 +21,11 @@ const Start = ({ fn }) => (
         <small>a very bad game from Charmed.Tech</small>
       </h2>
     </header>
-    <img alt="hero" style={{ height: 150 }} src={heroine} />
+    <div className="character-portraits">
+      <img alt="hero" className="hero-image" src={heroine} />
+      <img alt="enemy boss" className="boss-image" src={boss} />
+      <img alt="Prince Few" className="few-image" src={few} />
+    </div>
     <h4>Controls:</h4>
     <h5>Spacebar changes weapons</h5>
     <h5>WASD or arrow keys to move and attack</h5>
