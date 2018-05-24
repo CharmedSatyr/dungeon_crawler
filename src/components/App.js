3d0985eda4ed5e5acc23af333226a023cb4a0e2a
CharmedSatyr <z@charmed.tech>
2018-05-24 12:21:44 +0700

    css -> scss

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import './App.css';
 
 import Game from '../containers/Game';
 
 const App = () => (
   <div className="App">
-    <div className="container">
-      <Game />
-    </div>
+    <Game />
   </div>
 );
 
