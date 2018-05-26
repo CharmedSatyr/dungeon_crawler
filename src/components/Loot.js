aedbae812df000c5a5b2020bdda95e222d525a5e
CharmedSatyr <z@charmed.tech>
2018-05-26 12:59:38 +0700

    +gold background positions

--- src/components/Loot.js
+++ src/components/Loot.js
@@ -5,13 +5,29 @@ import PropTypes from 'prop-types';
 
 import tiles from '../resources/loot/Loot.png';
 
+/*** Loot Spritesheet Background Positions ***/
+// Barrels
 export const emptyBarrelPosition = '0 0';
 export const fullBarrelPosition = `${-c.CELL_SIDE}px 0`;
+
+// Weapons
 export const spearPosition = `0 ${-c.CELL_SIDE}px`;
 export const dragonSpearPosition = `${-c.CELL_SIDE}px ${-c.CELL_SIDE}px`;
+
+// Gold
+export const goldCoinPosition = `0 ${-3 * c.CELL_SIDE}px`;
+export const goldHandfulPosition = `${-c.CELL_SIDE}px ${-3 * c.CELL_SIDE}px`;
+export const goldNuggetPosition = `${-2 * c.CELL_SIDE}px ${-3 * c.CELL_SIDE}px`;
+export const goldSmPilePosition = `${-3 * c.CELL_SIDE}px ${-3 * c.CELL_SIDE}px`;
+export const goldPilePosition = `${-4 * c.CELL_SIDE}px ${-3 * c.CELL_SIDE}px`;
+export const goldPouchPosition = `${-5 * c.CELL_SIDE}px ${-3 * c.CELL_SIDE}px`;
+export const goldStashPosition = `${-6 * c.CELL_SIDE}px ${-3 * c.CELL_SIDE}px`;
+
+// Fallback
 export const warningSquarePosition = `0 ${-c.CELL_SIDE}px`;
 
 export const setBGPosition = variety => {
+  // Barrels
   if (variety === l.fullBarrel) {
     return fullBarrelPosition;
   }
@@ -20,12 +36,42 @@ export const setBGPosition = variety => {
     return emptyBarrelPosition;
   }
 
+  // Weapons
   if (variety.item && variety.item === l.weapons.spear) {
     return spearPosition;
   }
 
   if (variety.item && variety.item === l.weapons.dragonSpear) {
     return dragonSpearPosition;
+  }
+
+  // Gold
+  if (variety === l.gold.coin) {
+    return goldCoinPosition;
+  }
+
+  if (variety === l.gold.handful) {
+    return goldHandfulPosition;
+  }
+
+  if (variety === l.gold.nugget) {
+    return goldNuggetPosition;
+  }
+
+  if (variety === l.gold.sm_pile) {
+    return goldSmPilePosition;
+  }
+
+  if (variety === l.gold.pile) {
+    return goldPilePosition;
+  }
+
+  if (variety === l.gold.pouch) {
+    return goldPouchPosition;
+  }
+
+  if (variety === l.gold.stash) {
+    return goldStashPosition;
   }
 
   return warningSquarePosition;
