173e09ec51913f30ff34c22bfbed8ccf37377874
CharmedSatyr <z@charmed.tech>
2018-05-26 13:04:26 +0700

    +full tests for gold background positions

--- src/components/__tests__/Loot.test.js
+++ src/components/__tests__/Loot.test.js
@@ -7,6 +7,7 @@ import * as l from '../../constants/loot';
 import * as lc from '../Loot';
 
 describe('`setVariety` Loot component function', () => {
+  // Barrels
   it('should return `fullBarrel` position if `variety` has a full barrel payload', () => {
     const variety = l.fullBarrel;
     expect(lc.setBGPosition(variety)).toEqual(lc.fullBarrelPosition);
@@ -15,6 +16,36 @@ describe('`setVariety` Loot component function', () => {
   it('should return `emptyBarrel` position if `variety` has an empty barrel payload', () => {
     const variety = l.emptyBarrel;
     expect(lc.setBGPosition(variety)).toEqual(lc.emptyBarrelPosition);
+  });
+
+  // Gold
+  it('should return `goldCoinPosition` if `variety` has a gold coin payload', () => {
+    const variety = l.gold.coin;
+    expect(lc.setBGPosition(variety)).toEqual(lc.goldCoinPosition);
+  });
+  it('should return `goldHandfulPosition` if `variety` has a gold handful payload', () => {
+    const variety = l.gold.handful;
+    expect(lc.setBGPosition(variety)).toEqual(lc.goldHandfulPosition);
+  });
+  it('should return `goldNuggetPosition` if `variety` has a gold nugget payload', () => {
+    const variety = l.gold.nugget;
+    expect(lc.setBGPosition(variety)).toEqual(lc.goldNuggetPosition);
+  });
+  it('should return `goldSmPilePosition` if `variety` has a gold small pile payload', () => {
+    const variety = l.gold.sm_pile;
+    expect(lc.setBGPosition(variety)).toEqual(lc.goldSmPilePosition);
+  });
+  it('should return `goldPilePosition` if `variety` has a gold pile payload', () => {
+    const variety = l.gold.pile;
+    expect(lc.setBGPosition(variety)).toEqual(lc.goldPilePosition);
+  });
+  it('should return `goldPouchPosition` if `variety` has a gold pouch payload', () => {
+    const variety = l.gold.pouch;
+    expect(lc.setBGPosition(variety)).toEqual(lc.goldPouchPosition);
+  });
+  it('should return `goldStashPosition` if `variety` has a gold stash payload', () => {
+    const variety = l.gold.stash;
+    expect(lc.setBGPosition(variety)).toEqual(lc.goldStashPosition);
   });
 
   it('should return a `spear` position if `variety` has a spear payload', () => {
