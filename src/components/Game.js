6422b8ac4e1a600f963a766c2fdd28b7e594d762
CharmedSatyr <z@charmed.tech>
2018-06-15 05:36:28 +0700

    +set width

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import * as c from '../constants/settings';
+
 import PlayerPanel from './PlayerPanel';
 import Messages from './Messages';
 import Map from './Map';
 
 const Game = ({ cells, messages, player }) => (
-  <div>
+  <div
+    style={{
+      width: c.CELL_SIDE * c.VIEW_WIDTH,
+    }}
+  >
     <PlayerPanel stats={player} />
     <Messages messages={messages} />
     <Map cells={cells} />
