b049df55c95606ff612eee9a1f5ba5fd78fb5c1e
CharmedSatyr <z@charmed.tech>
2018-06-15 10:49:39 +0700

    +variable-based width

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,15 +2,13 @@ import React from 'react';
 import * as c from '../constants/settings';
 import PropTypes from 'prop-types';
 
-// Map width must be Cell width * GRID_WIDTH for columns to line up
-// UPDATE: Map should be 9 x 14 cells like Start
 const Map = ({ cells }) => (
   <div
     style={{
       display: 'flex',
       flexWrap: 'wrap',
-      height: c.CELL_SIDE * 9,
-      width: c.CELL_SIDE * 14,
+      height: c.CELL_SIDE * c.VIEW_HEIGHT,
+      width: c.CELL_SIDE * c.VIEW_WIDTH,
     }}
   >
     {cells}
