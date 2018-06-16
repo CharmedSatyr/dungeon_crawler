49d6f22b7f1c56ec7dd270e1cb4825f08ee4cda5
CharmedSatyr <z@charmed.tech>
2018-06-16 19:27:25 +0700

    style using variables; include nonfunctional button

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
-const GameOver = ({ win }) => (
-  <div>
+import * as c from '../constants/settings';
+
+const GameOver = ({ fn, win }) => (
+  <div
+    style={{
+      backgroundColor: 'red',
+      border: '1px solid red',
+      height: c.CELL_SIDE * c.VIEW_HEIGHT,
+      width: c.CELL_SIDE * c.VIEW_WIDTH,
+    }}
+  >
     <h2>GAME OVER</h2>
     <h4>
       {win
         ? 'Congratulations! You have rescued Prince Few!'
         : "You have failed your mission. Prince Few's life is surely forfeit."}
     </h4>
+    <button onClick={fn}>Try Again</button>
   </div>
 );
 
